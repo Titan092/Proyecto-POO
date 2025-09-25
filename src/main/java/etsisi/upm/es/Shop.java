2d30e14a883c2571432f8480dd8512657864aed0
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-09-25 18:25:26 +0200

    Prueba

--- src/main/java/etsisi/upm/es/Shop.java
+++ src/main/java/etsisi/upm/es/Shop.java
@@ -3,7 +3,7 @@ package etsisi.upm.es;
 /**
  * Hello world!
  */
-public class Tienda {
+public class Shop {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
