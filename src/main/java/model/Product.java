61c58bfccce75fcdfb1a110a02b93385d9fc9224
Titan092 <ftitan092@gmail.com>
2025-09-26 09:42:07 +0200

    patatudo

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,22 @@
 package model;
 
 public class Product {
+
+    private int id;
+    private String name;
+    private Category category;
+    private float price;
+
+    Product (int id, String name, Category category, float price){
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public void prodAdd (int id, String name, Category category, float price){
+
+    }
+
+
 }
