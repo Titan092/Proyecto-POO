dba1aee75e29e101c6f9ebc16f5c3d6c6ad2a563
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-10-01 10:49:53 +0200

    Command handler update

--- src/main/java/etsisi/upm/es/CommandHandler.java
+++ src/main/java/etsisi/upm/es/CommandHandler.java
@@ -8,16 +8,16 @@ import java.util.Scanner;
 
 public class CommandHandler {
 
+    private  final String ERRORMESSAGE="There has been a typo error, please, try again";
+    private Ticket ticket;
     protected void init() {
-        System.out.println("Welcome to the ticket module App.");
         System.out.println("Ticket module. Type 'help' to see commands.");
+        ticket=new Ticket();
     }
 
 
     protected void start() {
         Scanner sc=new Scanner(System.in);
-        String errorMessage="There has been a typo error, please, try again";
-        Ticket ticket = new Ticket();
         boolean continuar=true;
         while(continuar){
             System.out.print("tUPM> ");
@@ -36,17 +36,17 @@ public class CommandHandler {
                                 Category category=Category.valueOf(extra[0]);
                                 Product product=new Product(Integer.parseInt(comandoUni[2]), parts[1], category, Float.parseFloat(extra[1]));
                                 //Add to the ProductService object
-                            } else System.out.println(errorMessage);
+                            } else System.out.println(ERRORMESSAGE);
                             break;
                         case "list":
                             if(comandoUni.length==2){
-                                //Code
-                            } else System.out.println(errorMessage);
+                                //Call the ProductService object
+                            } else System.out.println(ERRORMESSAGE);
                             break;
                         case "update":
                             if(comandoUni.length==5){
                                 //Code
-                            } else System.out.println(errorMessage);
+                            } else System.out.println(ERRORMESSAGE);
                             break;
                         case "remove":
                             //Code5
