170353802b0d5aee962b92f9f48902855f201694
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-10-02 20:35:39 +0200

    Command handler update ProductService mini-update Commentaries in ProductService

--- src/main/java/etsisi/upm/es/Shop.java
+++ src/main/java/etsisi/upm/es/Shop.java
@@ -1,13 +1,29 @@
 package etsisi.upm.es;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Hello world!
  */
 public class Shop {
     public static void main(String[] args) {
-        CommandHandler command=new CommandHandler();
-        command.init();
-        command.start();
-        command.end();
+        try{
+            FileInputStream file=new FileInputStream("src/main/resources/input.txt");
+            System.setIn(file);
+
+            CommandHandler command=new CommandHandler();
+            command.init();
+            command.start();
+            command.end();
+
+            file.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Error: "+e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+
     }
 }
