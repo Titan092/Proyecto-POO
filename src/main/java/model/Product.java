276e9ee05b1f84f7c37e280cff7399c8df6bc0ef
Serevine <167626703+Serevine@users.noreply.github.com>
2025-10-07 12:18:51 +0200

    Formateo Season 3 Part I.

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -9,55 +9,86 @@ public class Product {
 
 
     /**
-     * Product Constructor
-     * @param id
-     * @param name
-     * @param category
-     * @param price
+     * Product contructor.
+     * @param id Unique ID.
+     * @param name Name of the product.
+     * @param category {@link Category} of the product.
+     * @param price Price of the product.
      */
-    public Product (int id, String name, Category category, float price){
+    public Product(int id, String name, Category category, float price) {
         this.id = id;
         this.name = name;
         this.category = category;
         this.price = price;
     }
 
+    /**
+     * Get ID.
+     * @return ID.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Get {@link Category}.
+     * @return Category.
+     */
     public Category getCategory() {
         return category;
     }
 
-
+    /**
+     * Get price.
+     * @return Price.
+     */
     public float getPrice() {
         return price;
     }
 
+    /**
+     * Get name.
+     * @return Name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set {@link Category}.
+     * @param category Category.
+     */
     public void setCategory(Category category) {
         this.category = category;
     }
 
+    /**
+     * Set name.
+     * @param name Name.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Set price.
+     * @param price Price.
+     */
     public void setPrice(float price) {
         this.price = price;
     }
 
+    /**
+     * Set ID.
+     * @param id ID.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
     /**
-     * Returns the String with the requested format
-     * @return
+     * Returns the String with the requested format.
+     * @return The formatted string.
      */
     @Override
     public String toString() {
