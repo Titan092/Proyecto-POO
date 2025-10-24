ffaf1c70a14f9f636ecd997d1aa24aa89d54c26b
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-10-24 10:04:25 +0200

    Cleaner tests update

--- src/main/java/etsisi/upm/es/Shop.java
+++ src/main/java/etsisi/upm/es/Shop.java
@@ -1,10 +1,29 @@
 package etsisi.upm.es;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 
 public class Shop {
     public static void main(String[] args) {
-        CommandHandler command=new CommandHandler();
-        command.init();
-        command.start();
-        command.end();
+        if(args.length>0){
+            String filePath= args[0];
+            try{
+                FileInputStream file=new FileInputStream(filePath);
+                System.setIn(file);
+                CommandHandler command=new CommandHandler();
+                command.init();
+                command.start();
+                command.end();
+                file.close();
+            }catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }else{
+            CommandHandler command=new CommandHandler();
+            command.init();
+            command.start();
+            command.end();
+        }
+
     }
 }
