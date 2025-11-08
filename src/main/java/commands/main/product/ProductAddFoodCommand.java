4230b3b8d7fe3476be86354b198decbeff352ffb
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-11-08 18:32:07 +0100

    Commands update

--- src/main/java/commands/main/product/ProductAddFoodCommand.java
+++ src/main/java/commands/main/product/ProductAddFoodCommand.java
@@ -1,12 +1,43 @@
 package commands.main.product;
 
 import commands.Command;
+import model.products.Category;
 import model.products.ProductService;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProductAddFoodCommand extends Command {
     private ProductService productService;
     public ProductAddFoodCommand(ProductService productService) {
         super("addFood");
         this.productService = productService;
+    }
+
+    public boolean apply(String[] args) {
+        boolean result = false;
+        if (args.length > 0 && args[1].equals(this.getName())) {
+            Pattern p=Pattern.compile("^prod addFood(?: (\\d+))? \"([^\"]+)\" ([\\d]+(?:\\.[\\d]+)?) (\\d{4}-\\d{2}-\\d{2}) (\\d+)$");
+            Matcher m=p.matcher(args.toString());
+            if(m.matches()){
+                boolean callWithId=false;
+                int id=0;
+                if(m.group(1)!=null){
+                    id = Integer.parseInt(m.group(1));
+                    callWithId=true;
+                }
+                String name = m.group(2);
+                float price =Float.parseFloat(m.group(3));
+                LocalDate expirationDate = LocalDate.parse(m.group(4));
+                int maxPers = Integer.parseInt(m.group(5));
+                if(callWithId) productService.prodAddFood(id, name, price, expirationDate, maxPers);
+                else productService.prodAddFood(name, price, expirationDate, maxPers);
+
+                result=true;
+            }
+        }
+        return result;
     }
 }
