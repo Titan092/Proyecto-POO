d8b52c17114b2f61f8ccf26fd807ac09532428e7
Titan092 <ftitan092@gmail.com>
2025-11-09 19:51:08 +0100

    patatudo

--- src/main/java/model/users/UserService.java
+++ src/main/java/model/users/UserService.java
@@ -44,28 +44,26 @@ public class UserService {
     }
 
     public void clientList(){
-        //ARREGLAR ESTO MIAU
 
-        //Put the keys in an ArrayList
-        HashMap<String,Client> clientSortedByName = new HashMap<>();
-
-        List<String> clientNames = new ArrayList<>();
+        //Put the name and Dni in a list
+        List<String> clientNamesAndDni = new ArrayList<>();
         for (Map.Entry<String, IUser> entry : users.entrySet()){
           if (!Character.isDigit(entry.getKey().charAt(8))){ //if last character is not a number its a dni, so its a client
-              clientSortedByName.put(entry.getValue().getName(),(Client) entry.getValue());
-             clientNames.add(entry.getValue().getName());
+              String nameAndDni = (entry.getValue().getName())+" "+(entry.getValue().getId());
+              clientNamesAndDni.add(nameAndDni);
           }
         }
 
-        //Sort the keys alphabetically
-        Collections.sort(clientNames);
+        //Sort the names alphabetically
+        Collections.sort(clientNamesAndDni);
 
-        for (int i=0;i<clientNames.size();i++){
-            String clientName = clientNames.get(i);
-            System.out.println(clientName);
+        for (int i=0;i<clientNamesAndDni.size();i++){
+            //separate the names and dnis
+            String [] clientNamesAndDniSeparated = clientNamesAndDni.get(i).split(" ");
+            String clientName = clientNamesAndDniSeparated[0];
+            String clientDni = clientNamesAndDniSeparated[1];
+            System.out.println("Client name: "+clientName+" DNI: "+clientDni);
         }
-
-
     }
 
     //comand for Cash with random Id
@@ -100,6 +98,27 @@ public class UserService {
     }
 
     public void cashList(){
+        //Put the name and the cashId in a list
+        List<String> cashNameAndDni = new ArrayList<>();
+        for (Map.Entry<String, IUser> entry : users.entrySet()){
+            if (Character.isAlphabetic(0)){//if the first character is alphabetic (U from UW) is a chash
+                String nameAndCashId = (entry.getValue().getName())+" "+ (entry.getValue().getId());
+                cashNameAndDni.add(nameAndCashId);
+            }
+        }
+
+        //Sort the names alphabetically
+        Collections.sort(cashNameAndDni);
+
+        for (int i=0;i<cashNameAndDni.size();i++){
+            //separate the names and dnis
+            String [] cashNameAndIdSeparated = cashNameAndDni.get(i).split(" ");
+            String cashName = cashNameAndIdSeparated[0];
+            String cashId = cashNameAndIdSeparated[1];
+            System.out.println("Cash name: "+cashName+" Cash ID: "+cashId);
+        }
+
+
 
     }
 
