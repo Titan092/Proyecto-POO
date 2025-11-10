1e680423a4125a9823075bb648f8acc39c852575
Serevine <167626703+Serevine@users.noreply.github.com>
2025-11-10 18:41:31 +0100

    user ahora tiene tickets

--- src/main/java/model/users/User.java
+++ src/main/java/model/users/User.java
@@ -1,15 +1,22 @@
 package model.users;
 
+import model.tickets.Ticket;
+
+import java.util.ArrayList;
+
 public abstract class User implements IUser {
 
     private String id;
     private String name;
     private String email;
+    // Every user has a list of tickets associated with it
+    private ArrayList<Ticket> tickets;
 
     public User(String id, String name, String email){
         this.id = id;
         this.name = name;
         this.email = email;
+        this.tickets = new ArrayList<>();
     }
 
     public String getId(){
@@ -34,5 +41,9 @@ public abstract class User implements IUser {
 
     public void setEmail(String email){
         this.email = email;
+    }
+
+    public void insertTicket(Ticket ticket){
+        tickets.add(ticket);
     }
 }
