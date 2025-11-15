e9fa9b46272daee797df65851c3bb5538841e6f1
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-11-15 12:25:43 +0100

    Help Command Update

--- src/main/java/commands/main/HelpCommand.java
+++ src/main/java/commands/main/HelpCommand.java
@@ -10,15 +10,38 @@ public class HelpCommand extends Command {
     public boolean apply(String[] args) {
         boolean result = false;
         if (args.length == 1 && args[0].equals(this.getName())) {
-            printHelp();
+            this.setMessage(getHelp());
             result = true;
         }
         return result;
     }
 
-    public void printHelp() {
-        System.out.println("Help Command:");
-        System.out.println("Usage: help");
-        System.out.println("Displays a list of available commands and their descriptions.");
+    public String getHelp() {
+        String result="Commands:\n" +
+                "  client add \"<nombre>\" <DNI> <email> <cashId>\n" +
+                "  client remove <DNI>\n" +
+                "  client list\n" +
+                "  cash add [<id>] \"<nombre>\"<email>\n" +
+                "  cash remove <id>\n" +
+                "  cash list\n" +
+                "  cash tickets <id>\n" +
+                "  ticket new [<id>] <cashId> <userId>\n" +
+                "  ticket add <ticketId><cashId> <prodId> <amount> [--p<txt> --p<txt>] \n" +
+                "  ticket remove <ticketId><cashId> <prodId> \n" +
+                "  ticket print <ticketId> <cashId> \n" +
+                "  ticket list\n" +
+                "  prod add <id> \"<name>\" <category> <price>\n" +
+                "  prod update <id> NAME|CATEGORY|PRICE <value>\n" +
+                "  prod addFood [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>\n" +
+                "  prod addMeeting [<id>] \"<name>\" <price> <expiration:yyyy-MM-dd> <max_people>\n" +
+                "  prod list\n" +
+                "  prod remove <id>\n" +
+                "  help\n" +
+                "  echo “<text>” \n" +
+                "  exit\n" +
+                "\n" +
+                "Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS\n" +
+                "Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%, ELECTRONICS 3%.";
+        return result;
     }
 }
--- src/main/java/etsisi/upm/es/CommandHandler.java
+++ src/main/java/etsisi/upm/es/CommandHandler.java
@@ -1,5 +1,4 @@
 package etsisi.upm.es;
-
 import commands.Command;
 import commands.main.HelpCommand;
 import commands.main.UnknownCommand;
@@ -9,10 +8,8 @@ import commands.main.user.UserCommand;
 import model.products.ProductService;
 import model.tickets.TicketService;
 import model.users.UserService;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class CommandHandler {
 
@@ -29,10 +26,6 @@ public class CommandHandler {
     private UserService users;
     private List<Command> commands;
 
-    /**
-     * Prompt message
-     */
-    private final String PROMPT = "tUPM> ";
 
     /**
      * Initializes all the required components of the shop.
@@ -40,8 +33,6 @@ public class CommandHandler {
     protected String init() {
         String WELCOME_MESSAGE = "Welcome to the ticket module App.\n" +
                                                           "Ticket module. Type 'help' to see commands.";
-        //CLI.printMessage("Welcome to the ticket module App.");
-        //CLI.printMessage("Ticket module. Type 'help' to see commands.");
         productService = new ProductService();
         tickets = new TicketService();
         users = new UserService();
@@ -56,31 +47,6 @@ public class CommandHandler {
         commands.add(new TicketCommand(tickets, productService));
         commands.add(new HelpCommand());
         commands.add(new UserCommand(users));
-    }
-
-    /**
-     * Starts the execution of the shop.
-     */
-    protected void start() {
-        boolean continues = true;
-        while(continues) {
-            CLI.printPrompt(PROMPT);
-            //This part is to differentiate between interactive and non-interactive mode (in the tests)
-            //All this is for cleaner output in the tests
-            String command = CLI.readCommand();
-            CLI.printIfIsFileInput(command);
-            String[] commandUni = command.split(" ");
-            switch (commandUni[0]){
-                case "echo":
-                    echo(command);
-                    break;
-                case "exit":
-                    continues=false;
-                    break;
-                default:
-                    applyCommand(commandUni);
-            }
-        }
     }
 
 
@@ -98,62 +64,12 @@ public class CommandHandler {
         return null;
     }
 
-
-
-
-
-
-
-    /**
-     * Prints all the available commands.
-     */
-    private void printHelp() {
-        String SPACE="  ";
-        System.out.println("Commands: ");
-        System.out.println(SPACE + "prod add <id> \"<name>\" <category> <price> ");
-        System.out.println(SPACE + "prod list");
-        System.out.println(SPACE + "prod update <id> NAME|CATEGORY|PRICE <value>");
-        System.out.println(SPACE + "prod remove <id>");
-        System.out.println(SPACE + "ticket new");
-        System.out.println(SPACE + "ticket add <prodId> <quantity> ");
-        System.out.println(SPACE + "ticket remove <prodId>");
-        System.out.println(SPACE + "ticket print");
-        System.out.println(SPACE + "echo \"<texto>\"");
-        System.out.println(SPACE + "help");
-        System.out.println(SPACE + "exit");
-        System.out.println();
-        System.out.println("Categories: MERCH, STATIONERY, CLOTHES, BOOK, ELECTRONICS\n" +
-                "Discounts if there are ≥2 units in the category: MERCH 0%, STATIONERY 5%, CLOTHES 7%, BOOK 10%, ELECTRONICS 3%.");
-    }
-
-    /**
-     * In case the user enters an unknown command, prints a helpful message.
-     */
-    private String unknownCommand() {
-        String message="Your command is not contemplated.\n"+
-                       "Type \"help\" for the command guide";
-        //CLI.printMessage("Your command is not contemplated.");
-        //CLI.printMessage("Type \"help\" for the command guide");
-        return message;
-    }
-
-    /**
-     * Prints whatever the user had entered before.
-     * @param message Message the user entered.
-     */
-    protected void echo(String message){
-        CLI.printMessage(message);
-        //No se usa
-    }
-
     /**
      * Bye!
      */
     protected String end() {
         String GOODBYE_MESSAGE="Closing application.\n"+
                                "Goodbye!";
-        //CLI.printMessage("Closing application.");
-        //CLI.printMessage("Goodbye!");
         return GOODBYE_MESSAGE;
     }
 }
