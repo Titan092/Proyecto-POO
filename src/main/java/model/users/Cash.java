af360b6dfe9647f30eb400faef32e73d24428376
Titan092 <ftitan092@gmail.com>
2025-11-17 14:08:45 +0100

    patatudo

--- src/main/java/model/users/Cash.java
+++ src/main/java/model/users/Cash.java
@@ -13,30 +13,22 @@ public class Cash extends User {
     }
 
     public void deleteTickets(String cashID, HashMap<String, IUser> users){
-        if (cashID.charAt(0) != 'U'){
-            System.out.println("The id is not valid");
-            return;
-        }else{
-            for (Map.Entry<String, IUser> entry : users.entrySet()){
-                String key = entry.getKey();
-                if (!key.isEmpty() && !Character.isAlphabetic(key.charAt(0))) {
-                    Client client = (Client) entry.getValue();
-                    HashMap<String, Ticket> clientTickets = client.getTickets();
-                    for (Map.Entry<String, Ticket> ticketEntry : clientTickets.entrySet()){
-                        Ticket ticketClient = (Ticket) ticketEntry.getValue();
-                        for (Map.Entry<String, Ticket> cashTicketEntry : tickets.entrySet()){
-                            Ticket ticketCash = (Ticket) cashTicketEntry.getValue();
-                            if (ticketClient.getId() == ticketCash.getId()){
-                                clientTickets.remove(ticketClient.getId());
-                            }
+        for (Map.Entry<String, IUser> entry : users.entrySet()){
+            String key = entry.getKey();
+            if (!key.isEmpty() && !Character.isAlphabetic(key.charAt(0))) {
+                Client client = (Client) entry.getValue();
+                HashMap<String, Ticket> clientTickets = client.getTickets();
+                for (Map.Entry<String, Ticket> ticketEntry : clientTickets.entrySet()){
+                    Ticket ticketClient = ticketEntry.getValue();
+                    for (Map.Entry<String, Ticket> cashTicketEntry : tickets.entrySet()){
+                        Ticket ticketCash = cashTicketEntry.getValue();
+                        if (ticketClient.getId() == ticketCash.getId()){
+                            clientTickets.remove(ticketClient.getId());
                         }
-
-
                     }
                 }
             }
         }
-
     }
 
 }
