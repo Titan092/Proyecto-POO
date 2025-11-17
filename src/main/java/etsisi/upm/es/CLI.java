ec2aaf7268dae34cd509fbeebeb2b2fcba4e3ef0
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-11-17 17:29:06 +0100

    CLI update (ProductService)

--- src/main/java/etsisi/upm/es/CLI.java
+++ src/main/java/etsisi/upm/es/CLI.java
@@ -86,7 +86,8 @@ public class CLI implements ICLI{
     }
 
     protected void echo(String message){
-        CLI.printMessage(message);
+        String cleanedMessage=message.replaceFirst("echo ","");
+        CLI.printMessage(cleanedMessage);
     }
 
 }
