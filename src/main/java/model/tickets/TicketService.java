96401f1bd4b3aa78f982d7773cfdb93f1a921ade
d1s2f3 <delgadogonzalezvictor1@gmail.com>
2025-11-29 14:21:30 +0100

    Tests Done

--- src/main/java/model/tickets/TicketService.java
+++ src/main/java/model/tickets/TicketService.java
@@ -130,7 +130,6 @@ public class TicketService {
         HashMap<String, IUser> users = userService.getUsers();
         ArrayList<String> cashIDSorted = new ArrayList<>();
         for (Map.Entry<String, IUser> entry : users.entrySet()) {
-            if (entry)
             String key = entry.getKey();
             if (!key.isEmpty() && Character.isAlphabetic(key.charAt(0))) {//if the first character is alphabetic (U from UW) is a chash
                 cashIDSorted.add(entry.getValue().getId());
